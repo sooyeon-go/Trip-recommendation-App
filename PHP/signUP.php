8ac9c3d1d23bcaf805ad355d41cad388c21798a6
JiHye <ghaee200@gmail.com>
2020-04-20 22:08:27 +0900

    success to insert

--- PHP/signUP.php
+++ PHP/signUP.php
@@ -12,25 +12,29 @@ if (mysqli_connect_errno($con))
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 }
+//POST 값을 읽어온다.
+$name=isset($_POST['Name']) ? $_POST['Name'] : ''; 
+$age=isset($_POST['Age']) ? $_POST['Age'] : '';  
+$sex=isset($_POST['Sex']) ? $_POST['Sex'] : ''; 
+$email=isset($_POST['Email']) ? $_POST['Email'] : '';
+$pw=isset($_POST['Pw']) ? $_POST['Pw'] : '';
 
-$username = $_POST['Name'];
-$userage = $_POST['Age'];
-$usersex = $_POST['Sex'];
-$useremail = $_POST['Email'];
-$userpw = $_POST['Pw'];
+if ($name !="" and $age !="" and $sex !="" and $email !="" and $pw !=""){   
+  
+    $sql="insert into User_Info (u_name,age,sex,u_email,u_pw) values ('$name','$age','$sex','$email','$pw')";  
+    $result=mysqli_query($con,$sql);  
 
-$result = mysqli_query($con,"insert into User_Info (u_name,age,sex,u_email,u_pw) values ('$username','$userage','$usersex','$useremail',$userpw')");
-  if($result){
-
-    echo 'success';
-
-  }
-
-  else{
-
-    echo 'failure';
-
-  }
+    if($result){  
+       echo "'$email'을 추가했습니다.";  
+    }  
+    else{  
+       echo "SQL문 처리중 에러 발생 : "; 
+       echo mysqli_error($con);
+    } 
+ 
+} else {
+    echo "데이터를 입력하세요 ";
+}
 mysqli_close($con);
 
 ?>
