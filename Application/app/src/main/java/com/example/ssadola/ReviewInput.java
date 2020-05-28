c3241a5de3d1994c3fbca530e3d3ce93261a5b3f
sooyeon-go <gojjang385@naver.com>
2020-05-29 02:10:23 +0900

    Create ReviewInput.java

---
+++ Application/app/src/main/java/com/example/ssadola/ReviewInput.java
@@ -0,0 +1,14 @@
+package com.example.ssadola;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReviewInput extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reviewinput);
+    }
+}
