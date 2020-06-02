e8fe7fe35c5ba4ecf3e88a1ed61515472f4c8b78
sooyeon-go <gojjang385@naver.com>
2020-06-03 04:42:48 +0900

    Update Reviewresult.java

--- Application/app/src/main/java/com/example/ssadola/Reviewresult.java
+++ Application/app/src/main/java/com/example/ssadola/Reviewresult.java
@@ -5,7 +5,9 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.os.Bundle;
 import android.content.Intent;
-
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.ListView;
 
 public class ReviewResult extends AppCompatActivity {
     private static String pub_ip = "http://15.165.95.187/";
@@ -16,6 +18,7 @@ public class ReviewResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review_list);
         imageButton = (ImageButton)findViewById(R.id.imageButton);
+        
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
