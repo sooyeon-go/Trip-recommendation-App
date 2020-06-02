80cfb6acc04ecc86d322e70f8e4f260a150c7314
sooyeon-go <gojjang385@naver.com>
2020-06-03 04:07:46 +0900

    Update Reviewresult.java

--- Application/app/src/main/java/com/example/ssadola/Reviewresult.java
+++ Application/app/src/main/java/com/example/ssadola/Reviewresult.java
@@ -1,14 +1,27 @@
 package com.example.ssadola;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.os.Bundle;
+import android.content.Intent;
 
-public class Reviewresult extends AppCompatActivity {
+
+public class ReviewResult extends AppCompatActivity {
+    private static String pub_ip = "http://15.165.95.187/";
+    private ImageButton imageButton;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.review_list);
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ReviewResult.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
