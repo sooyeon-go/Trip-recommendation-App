10b5584a8329ea91d5df52c09ea5d3248339ac04
JiHye <ghaee200@gmail.com>
2020-06-03 22:57:21 +0900

    case return

--- Application/app/src/main/java/com/example/ssadola/MainActivity.java
+++ Application/app/src/main/java/com/example/ssadola/MainActivity.java
@@ -202,9 +202,11 @@ public class MainActivity extends AppCompatActivity implements ViewAnimator.View
             case ContentFragment.REVIEW_INPUT:
                 Intent review_input = new Intent(MainActivity.this,ReviewInput.class);
                 startActivity(review_input);
+                return null;
             case ContentFragment.REVIEW_RESULT:
                 Intent review_result= new Intent(MainActivity.this,ReviewResult.class);
                 startActivity(review_result);
+                return null;
             default:
                // Toast.makeText(MainActivity.this, position + "", Toast.LENGTH_LONG).show();
                 return replaceFragment(screenShotable, position);
