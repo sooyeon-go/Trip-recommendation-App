c8aa4d6d550612dd541fcd3531dcfe50f2be2304
sooyeon-go <gojjang385@naver.com>
2020-06-04 21:45:17 +0900

    Update review_list.xml

---
+++ Application/app/src/main/java/com/example/ssadola/PersonalData.java
@@ -0,0 +1,47 @@
+package com.example.ssadola;
+
+public class PersonalData {
+    private String member_hotel;
+    private String member_sight;
+    private String member_eat;
+    private String member_place;
+    private String member_rating;
+    private String member_spec;
+
+    public String getMember_hotel(){
+        return member_hotel;
+    }
+    public String getMember_sight(){
+        return member_sight;
+    }
+    public String getMember_eat(){
+        return member_eat;
+    }
+    public String getMember_place(){
+        return member_place;
+    }
+    public String getMember_rating(){
+        return member_rating;
+    }
+    public String getMember_spec(){
+        return member_spec;
+    }
+    public void setMember_hotel(String member_hotel) {
+        this.member_hotel = member_hotel;
+    }
+    public void setMember_sight(String member_sight) {
+        this.member_sight = member_sight;
+    }
+    public void setMember_eat(String member_eat) {
+        this.member_eat = member_eat;
+    }
+    public void setMember_place(String member_place) {
+        this.member_place = member_place;
+    }
+    public void setMember_rating(String member_rating) {
+        this.member_rating = member_rating;
+    }
+    public void setMember_spec(String member_spec) {
+        this.member_spec = member_spec;
+    }
+}
