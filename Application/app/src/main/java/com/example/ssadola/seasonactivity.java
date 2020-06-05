d32d524d2538410e262370a4d408be13471b0173
huisukim <4724080@naver.com>
2020-06-05 16:49:04 +0900

    season's festival pic

--- Application/app/src/main/java/com/example/ssadola/seasonactivity.java
+++ Application/app/src/main/java/com/example/ssadola/seasonactivity.java
@@ -1,7 +1,13 @@
 package com.example.ssadola;
 
 import android.app.AppComponentFactory;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +17,32 @@ public class seasonactivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_season);
+
     }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_season, container, false);
+
+        Button spring = rootView.findViewById(R.id.btn_spring);
+        spring.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          Intent truntospring = new Intent(getActivity(),TitleStudioActivity.class);
+                                          startActivity(truntospring);
+                                      }
+                                  }
+
+        );
+        
+
+
+
+
+        return rootView;
+    }
+
+
+
 
 }
