f5e9524a1be5d733efe0c7cbc71f72c712a870fd
JiHye <ghaee200@gmail.com>
2020-06-05 14:59:45 +0900

    delete comment

--- Application/app/src/main/java/com/example/ssadola/CardFragment.java
+++ Application/app/src/main/java/com/example/ssadola/CardFragment.java
@@ -36,7 +36,7 @@ public class CardFragment extends Fragment {
     static String pub_ip = "http://15.165.95.187/";
     private CardView cardView;
     StringBuilder sb;
-    ArrayList<String> title= new ArrayList<String> ();;
+    ArrayList<String> title= new ArrayList<String> ();
 
     public static Fragment getInstance(int position) {
         CardFragment f = new CardFragment();
@@ -57,96 +57,23 @@ public class CardFragment extends Fragment {
         mCardView = (CardView) view.findViewById(R.id.cardView);
         mCardView.setMaxCardElevation(mCardView.getCardElevation()
                 * CardAdapter.MAX_ELEVATION_FACTOR);
+
         return view;
-/*
-        cardView = (CardView) view.findViewById(R.id.cardView);
+
+        /*cardView = (CardView) view.findViewById(R.id.cardView);
         cardView.setMaxCardElevation(cardView.getCardElevation() * CardAdapter.MAX_ELEVATION_FACTOR);
 
-        TextView title = (TextView) view.findViewById(R.id.work_title);
-        Button button = (Button)view.findViewById(R.id.button);
+        TextView title = (TextView) view.findViewById(R.id.work_title);*/
+        //Button button = (Button)view.findViewById(R.id.button);
 
 //        title.setText(String.format("Card %d", getArguments().getInt("position")));
         //title.setText(String.format(get));
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Button in Card " + getArguments().getInt("position")
-                        + "Clicked!", Toast.LENGTH_SHORT).show();
-            }
-        });*/
 
         //return view;
     }
 
     public CardView getCardView() {
         return cardView;
-    }
-
-
-    private class GetStudioInfo extends AsyncTask<String, Void, String>{
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if(result == null){
-
-            }else{
-                try {
-                    JSONObject jsonObject = new JSONObject(result);
-                    JSONArray jsonArray = jsonObject.getJSONArray("Studio_Info");
-                    for(int i=0;i<jsonArray.length();i++){
-                        JSONObject item = jsonArray.getJSONObject(i);
-                        String i_title = item.getString("work_title");
-
-
-                        /*HashMap<String,String> hashMap = new HashMap<>();
-                        hashMap.put("work_title",i_title);*/
-                       title.add(i,i_title);
-                    }
-
-                } catch (JSONException e) {
-                    Log.d("BookmarkActivity", "showResult : ", e);
-                }
-            }
-        }
-        @Override
-        protected String doInBackground(String... strings) {
-            final String link = pub_ip + "Distinct_Studio.php";
-            try {
-
-                URL url = new URL(link);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.connect();
-
-                int responseStatusCode = conn.getResponseCode();
-                //Log.d(TAG, "response code - " + responseStatusCode);
-                InputStream inputStream;
-                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    inputStream =conn.getInputStream();
-                }
-                else {
-                    inputStream = conn.getErrorStream();
-                }
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-
-                sb = new StringBuilder();
-                String line = null;
-
-                // Read Server Response
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line);
-                }
-                reader.close();
-                return sb.toString();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return new String("Exception: " + e.getMessage());
-            }
-        }
     }
 
 }
