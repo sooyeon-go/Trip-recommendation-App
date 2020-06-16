9a369a7891eb0a476008be1cb0815d7ca0191733
JiHye <ghaee200@gmail.com>
2020-06-16 14:08:11 +0900

    insert recomm result

--- Application/app/src/main/java/com/example/ssadola/ResultDetailActivity.java
+++ Application/app/src/main/java/com/example/ssadola/ResultDetailActivity.java
@@ -3,10 +3,12 @@ package com.example.ssadola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
@@ -28,8 +30,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +51,8 @@ public class ResultDetailActivity extends AppCompatActivity implements OnMapRead
     Geocoder geocoder;
     String tmp;
     String[] course;
+    static String pub_ip = "http://15.165.95.187/";
+    ArrayList<HashMap<String, String>> arrayList;
     ArrayList<String> location = new ArrayList<>();
     ArrayList<LatLng> position = new ArrayList<>();
     ArrayList<LatLng> line_points = new ArrayList<>();
@@ -65,12 +79,28 @@ public class ResultDetailActivity extends AppCompatActivity implements OnMapRead
         mBookmark_detail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mBookmark_detail.isSelected()){
-                    mBookmark_detail.setSelected(false);
-                    //bookmark_delete.php
-                }else{
-                    mBookmark_detail.setSelected(true);
-                    //bookmark_insert.php
+                arrayList = GetLoginData();
+                if(arrayList == null){
+                    Toast.makeText(v.getContext(),"로그인 먼저 해주세요",Toast.LENGTH_LONG).show();
+                    Intent login = new Intent(v.getContext(),LoginActivity.class);
+                    startActivity(login);
+                    //v.getContext().finish();
+                }
+                else{
+                    HashMap<String,String> hashMap = arrayList.get(0);
+                    String u_email = hashMap.get("u_email");
+                    if(mBookmark_detail.isSelected()){
+                        mBookmark_detail.setSelected(false);
+                        //bookmark_delete.php
+                        Bookmark_delete delete = new Bookmark_delete();
+                        delete.execute(u_email);
+                    }else{
+                        mBookmark_detail.setSelected(true);
+                        //bookmark_insert.php
+
+                        Bookmark_insert insert = new Bookmark_insert();
+                        insert.execute(u_email);
+                    }
                 }
             }
         });
@@ -84,8 +114,19 @@ public class ResultDetailActivity extends AppCompatActivity implements OnMapRead
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_course);
         mapFragment.getMapAsync(this); //getMapAsync must be called on the main thread.
     }
-
-    @Override
+    public ArrayList<HashMap<String, String>> GetLoginData() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(ResultDetailActivity.this);
+        Gson gson = new Gson();
+        String json = sharedPrefs.getString("UserInfo", "EMPTY");
+        if (json.equals("EMPTY")) {
+            //Toast.makeText(BookmarkActivity.this,"로그인 먼저 해주세요",Toast.LENGTH_LONG).show();
+            return null;
+        }
+        Type type = new TypeToken<ArrayList<HashMap<String, String>>>() {
+        }.getType();
+        return gson.fromJson(json, type);
+    }
+        @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         geocoder = new Geocoder(this);
@@ -160,5 +201,131 @@ public class ResultDetailActivity extends AppCompatActivity implements OnMapRead
 
         list_address.add(address);
     }
+    class Bookmark_insert extends AsyncTask<String, Void, String> {
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            //loading.dismiss();
+            System.out.println("Result : " + result);
+            if(result!=null){
+                Toast.makeText(ResultDetailActivity.this,result,Toast.LENGTH_SHORT).show();
+            }else{
+                Log.e("ImageStudioFragment","result is null");
+            }
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                //u_email,i_title,i_scene,i_location,i_address,mImageUrl
+                String u_email = (String) params[0];
+                String title = (String) params[1];
+                String scene = params[2];
+                String location = params[3];
+                String address = params[4];
+                String image = params[5];
 
+                String link = pub_ip+"Bookmark_insert.php";
+                //String data = URLEncoder.encode("Email", "UTF-8") + "=" + URLEncoder.encode(Email, "UTF-8");
+                //data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                String data = "u_email="+u_email+"&title="+ URLEncoder.encode(title,"UTF-8")
+                        + "&scene="+URLEncoder.encode(scene,"UTF-8") + "&location="+URLEncoder.encode(location,"UTF-8")
+                        +"&address="+URLEncoder.encode(address,"UTF-8")+"&image="+URLEncoder.encode(image,"UTF-8");
+
+                URL url = new URL(link);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.connect();
+
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                wr.close();
+                int responseStatusCode = conn.getResponseCode();
+                Log.d("Bookmark_insert", "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream =conn.getInputStream();
+                }
+                else{
+                    inputStream = conn.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                reader.close();
+                return sb.toString();
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+    }
+    class Bookmark_delete extends AsyncTask<String, Void, String> {
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            //loading.dismiss();
+            System.out.println("Result : " + result);
+            if(result!=null){
+                Toast.makeText(ResultDetailActivity.this,result,Toast.LENGTH_SHORT).show();
+            }else{
+                Log.e("ImageStudioFragment","result is null");
+            }
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                //u_email,i_title,i_scene,i_location,i_address,mImageUrl
+                String u_email = (String) params[0];
+                String title = (String) params[1];
+                String location = params[2];
+
+
+                String link = pub_ip+"Bookmark_delete.php";
+                //String data = URLEncoder.encode("Email", "UTF-8") + "=" + URLEncoder.encode(Email, "UTF-8");
+                //data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                String data = "u_email="+u_email+"&title="+URLEncoder.encode(title,"UTF-8")
+                        + "&location="+URLEncoder.encode(location,"UTF-8");
+
+                URL url = new URL(link);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.connect();
+
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                wr.close();
+                int responseStatusCode = conn.getResponseCode();
+                Log.d("Bookmark_insert", "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream =conn.getInputStream();
+                }
+                else{
+                    inputStream = conn.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                reader.close();
+                return sb.toString();
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+    }
 }
